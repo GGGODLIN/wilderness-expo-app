48f5b053ba5a67dd2442144b84df6c997b57e3af
Lanar <lanar@estiginto.com>
2022-09-10 16:35:30 +0000

    feat: add button

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -5,7 +5,7 @@ import { Avatar } from 'react-native-elements';
 import avatars from '../assets/avatars';
 import { api, dispatchFetchRequest } from "../constants/Backend";
 import colors from '../constants/Colors';
-import { ListItem, Icon, Header, Card, Divider } from '@rneui/themed'
+import { ListItem, Icon, Header, Card, Divider, SpeedDial } from '@rneui/themed'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome5';
 import MasonryList from '@react-native-seoul/masonry-list';
@@ -13,279 +13,7 @@ import MapView from 'react-native-maps';
 
 const LocationScreen = ({ navigation, route }) => {
 
-    const [userProfile, setUserProfile] = useState(null);
-
-    useFocusEffect(
-        React.useCallback(() => {
-            getUserProfile()
-
-            return () => { };
-        }, [])
-    );
-
-
-    const getUserProfile = () => {
-        dispatchFetchRequest(
-            api.user.userProfile,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                },
-            },
-            response => {
-                response.json().then(data => {
-                    setUserProfile(data?.data)
-                })
-            },
-            response => {
-                response.text().then(data => {
-                    console.warn('getUserProfileFail', data)
-                })
-            }
-        ).then()
-    }
-
-    
-
-    const iconSize = 32;
-    const iconStyle = { color: colors?.InactiveColor };
-    const titleStyle = { color: '#666666', fontSize: 16 };
-    const indexLinkList = [
-        {
-            icon: <FontAwesome name="caravan" size={iconSize} style={iconStyle} />,
-            title: '車泊熱點',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="snowboarding" size={iconSize} style={iconStyle} />,
-            title: 'SUP秘境',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="campground" size={iconSize} style={iconStyle} />,
-            title: '熱門營地',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="fish" size={iconSize} style={iconStyle} />,
-            title: '親子營區',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="icons" size={iconSize} style={iconStyle} />,
-            title: '熱門文章',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="drumstick-bite" size={iconSize} style={iconStyle} />,
-            title: '露營美食',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="map-signs" size={iconSize} style={iconStyle} />,
-            title: '好野入門',
-            component: <></>,
-        },
-        {
-            icon: <FontAwesome name="question-circle" size={iconSize} style={iconStyle} />,
-            title: '常見問題',
-            component: <></>,
-        },
-    ]
-
-    const indexLinkListItem = ({ item }) => {
-        return (
-            <View style={{ alignItems: 'center', paddingLeft: 15, paddingRight: 15, paddingTop: 20, paddingBottom: 0 }}>
-                <TouchableOpacity>
-                    <View style={{ alignItems: 'center', paddingTop: 0, paddingBottom: 10 }}>
-                        {item.icon}
-                    </View>
-                    <View>
-                        <TouchableOpacity>
-                            <Text style={titleStyle}>{item.title}</Text>
-                        </TouchableOpacity>
-                    </View>
-                </TouchableOpacity>
-            </View>
-        );
-    };
-    
-    const indexArticleList = [
-        {
-            title: '露營分享1',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_1.jpg'),
-            countViews: 125,
-            component: <></>,
-        },
-        {
-            title: '露營分享2',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_2.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享3',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_3.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_4.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_5.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_6.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-    ];
-
-    const indexWaterfallList = [
-        {
-            title: '露營分享1',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_1.jpg'),
-            countViews: 125,
-            component: <></>,
-        },
-        {
-            title: '露營分享2',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_2.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享3',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_3.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_4.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_5.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_6.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_7.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_8.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_9.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-        {
-            title: '露營分享',
-            date: '2022-09-01',
-            image: require('../assets/images/views/view_10.jpg'),
-            countViews: 6521, 
-            component: <></>,
-        },
-
-
-
-    ]
-
-    const indexWaterfallListItem = ({ item }) => {
-        return (
-            <>
-                <Image
-                    style={{ width: 100 }}
-                    resizeMode='contain'
-                    source={ item.image }
-                />
-                <Text style={{ fontSize: 16, color: '#666', }} color={'#999999'}>{item.title}</Text>
-            </>
-        );
-    };
-
-    const articleImages = ({ item }) => {
-        return (
-            <>
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                {/*
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                <Image
-                    style={styles.image}
-                    resizeMode="cover"
-                    source={item.image}
-                />
-                */}
-            </>
-        );
-    };
-    
-
+    const [open, setOpen] = React.useState(false);
 
     return (
         <View style={{ flex: 1, padding: 0, backgroundColor: '#fff', borderTopWidth: 2, borderColor: colors?.InactiveColor }}>
@@ -300,6 +28,29 @@ const LocationScreen = ({ navigation, route }) => {
                     }}
                 />
             </View>
+                <SpeedDial
+                    isOpen={open}
+                    icon={{ name: 'menu', color: '#fff' }}
+                    openIcon={{ name: 'close', color: '#fff' }}
+                    onOpen={() => setOpen(!open)}
+                    onClose={() => setOpen(!open)}
+                    color='#333'
+                    top={10}
+                    style={[{ left: 0, top: 0 }]}
+                >
+                    <SpeedDial.Action
+                        icon={{ name: 'add', color: '#fff' }}
+                        title="新增地點"
+                        onPress={() => console.log('Add Something')}
+                        color='#333'
+                    />
+                    <SpeedDial.Action
+                        icon={{ name: 'delete', color: '#fff' }}
+                        title="Delete"
+                        onPress={() => console.log('Delete Something')}
+                        color='#333'
+                    />
+                </SpeedDial>
         </View>
     );
 }

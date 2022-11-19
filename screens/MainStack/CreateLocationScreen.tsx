649304d21cad527eded259288ba8141f33f2e358
Lanar <arhspt8011@gmail.com>
2022-11-19 21:03:22 +0800

    create location

--- screens/MainStack/CreateLocationScreen.tsx
+++ screens/MainStack/CreateLocationScreen.tsx
@@ -1,4 +1,6 @@
 import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+import MasonryList from '@react-native-seoul/masonry-list';
 import * as ImagePicker from 'expo-image-picker';
 import {
   Button,
@@ -14,10 +16,13 @@ import {
   Image,
   Stack,
   IconButton,
+  //Select,
+  CheckIcon,
 } from 'native-base';
 import React, { useState } from 'react';
+import { Keyboard, Dimensions, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 
-import { MAIN_STACK_POST } from '../../NavigationNames';
+import { MAIN_STACK_LOCATION_DRAWER } from '../../NavigationNames';
 import { NavigationProps } from '../../Props';
 import Colors from '../../constants/Colors';
 import DashboardLayout from '../../layouts/DashboardLayout';
@@ -65,6 +70,15 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
     email: '',
     password: '',
   });
+
+  const [showCountry, setShowCountry] = useState(false);
+  const [showAltitude, setShowAltitude] = useState(false);
+  const [showRecommend, setShowRecommend] = useState(false);
+  const [showCrowded, setShowCrowded] = useState(false);
+  const [country, setCountry] = React.useState('');
+  const [altitude, setAltitude] = useState('');
+  const [recommend, setRecommend] = useState('');
+  const [crowded, setCrowded] = useState('');
 
   function PlanDescriptionItem({ planTitle }: { planTitle: string }) {
     return (
@@ -280,50 +294,349 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
   type Icon = {
     name: string;
     text: string;
-    active: boolean;
+    state: number;
   };
   const icons: Icon[] = [
     {
       name: 'caravan',
       text: '收費營地',
-      active: true,
+      state: 0,
     },
     {
       name: 'caravan',
       text: '公共營地',
-      active: false,
+      state: 0,
     },
     {
       name: 'caravan',
       text: '露營車',
-      active: false,
+      state: 0,
     },
     {
       name: 'caravan',
       text: '露營車',
-      active: false,
+      state: 0,
     },
     {
       name: 'caravan',
-      text: '無提供',
-      active: false,
+      text: '任意',
+      state: 0,
     },
     {
       name: 'caravan',
-      text: '有提供',
-      active: true,
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
+    },
+    {
+      name: 'caravan',
+      text: '任意',
+      state: 0,
     },
   ];
 
+  function getFacilityStyle(state: number) {
+    if (state === 0) {
+      return 'coolGray.200';
+    }
+    if (state === 1) {
+      return 'coolGray.700';
+    }
+    if (state === 2) {
+      return 'red.700';
+    }
+    return 'coolGray.500';
+  }
+  function FacilityCard(props: { item: Icon }) {
+    const [facilityState, setFacilityState] = useState(props.item.state);
+
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          if (facilityState >= 2) {
+            setFacilityState(0);
+          } else {
+            setFacilityState(facilityState + 1);
+          }
+        }}>
+        <HStack
+          key={'icon_' + props.index}
+          overflow="visible"
+          mx={0}
+          mb={0}
+          alignItems="center"
+          justifyContent="flex-start">
+          <IconButton
+            variant="unstyled"
+            icon={
+              <Icon
+                as={FontAwesome5}
+                name={props.item.name}
+                _light={{
+                  color: getFacilityStyle(facilityState),
+                }}
+                size={3}
+                textAlign="center"
+                alignSelf="center"
+              />
+            }
+          />
+          <Text
+            fontSize="sm"
+            color={getFacilityStyle(facilityState)}
+            textAlign="center"
+            strikeThrough={facilityState === 2}>
+            {props.item.text}
+          </Text>
+        </HStack>
+      </TouchableOpacity>
+    );
+  }
+
   return (
     <DashboardLayout title="新增地點" showBackButton>
-      <Box
-        px={{ md: 0, xl: 35 }}
-        py={{ md: 8 }}
-        flex={1}
-        _light={{ bg: 'white' }}
-        _dark={{ bg: 'coolGray.800' }}>
-        <VStack space="0" px="4" py="4">
+      {showCountry && (
+        <VStack
+          position="absolute"
+          bg="white"
+          bottom={0}
+          left={0}
+          w="100%"
+          h="240"
+          pb="30"
+          shadow={2}
+          zIndex={9}
+          flex={1}
+          justifyContent="center">
+          <Picker
+            mode="dialog"
+            style={{ height: 100, flex: 1 }}
+            accessibilityLabel="選擇縣市"
+            placeholder="選擇縣市"
+            selectedValue={country}
+            onValueChange={(itemValue, itemIndex) => {
+              setCountry(itemValue);
+            }}>
+            <Picker.Item label="台北市" value="台北" />
+            <Picker.Item label="新北市" value="新北" />
+            <Picker.Item label="桃園市" value="桃園" />
+            <Picker.Item label="台中市" value="台中" />
+            <Picker.Item label="待串接整合縣市" value="" />
+          </Picker>
+          <Button
+            mx={4}
+            variant="solid"
+            size="lg"
+            style={{ backgroundColor: 'black' }}
+            onPress={() => {
+              setShowCountry(false);
+            }}>
+            選擇
+          </Button>
+        </VStack>
+      )}
+      {showAltitude && (
+        <VStack
+          position="absolute"
+          bg="white"
+          bottom={0}
+          left={0}
+          w="100%"
+          h="240"
+          pb="30"
+          shadow={2}
+          zIndex={9}
+          flex={1}
+          justifyContent="center">
+          <Picker
+            mode="dialog"
+            style={{ height: 100, flex: 1 }}
+            accessibilityLabel="選擇海拔"
+            placeholder="選擇海拔"
+            selectedValue={altitude}
+            onValueChange={(itemValue, itemIndex) => {
+              setAltitude(itemValue);
+            }}>
+            <Picker.Item label="不確定" value="" />
+            <Picker.Item label="海邊" value="海邊" />
+            <Picker.Item label="平地" value="平地" />
+            <Picker.Item label="300公尺以下" value="300" />
+            <Picker.Item label="300公尺~500公尺" value="500" />
+            <Picker.Item label="500公尺~800公尺" value="800" />
+            <Picker.Item label="800公尺~1000公尺" value="1000" />
+            <Picker.Item label="1000公尺以上" value="1100" />
+          </Picker>
+          <Button
+            mx={4}
+            variant="solid"
+            size="lg"
+            style={{ backgroundColor: 'black' }}
+            onPress={() => {
+              setShowAltitude(false);
+            }}>
+            選擇
+          </Button>
+        </VStack>
+      )}
+      {showCrowded && (
+        <VStack
+          position="absolute"
+          bg="white"
+          bottom={0}
+          left={0}
+          w="100%"
+          h="240"
+          pb="30"
+          shadow={2}
+          zIndex={9}
+          flex={1}
+          justifyContent="center">
+          <Picker
+            mode="dialog"
+            style={{ height: 100, flex: 1 }}
+            accessibilityLabel="選擇擁擠程度"
+            placeholder="選擇擁擠程度"
+            selectedValue={crowded}
+            onValueChange={(itemValue, itemIndex) => {
+              setCrowded(itemValue);
+            }}>
+            <Picker.Item label="空曠" value="空曠" />
+            <Picker.Item label="偶爾" value="偶爾" />
+            <Picker.Item label="總是" value="總是" />
+          </Picker>
+          <Button
+            mx={4}
+            variant="solid"
+            size="lg"
+            style={{ backgroundColor: 'black' }}
+            onPress={() => {
+              setShowCrowded(false);
+            }}>
+            選擇
+          </Button>
+        </VStack>
+      )}
+      <ScrollView
+        onPress={() => {
+          Keyboard.dismiss();
+          setShowCountry(false);
+        }}>
+        <VStack
+          space="0"
+          px="4"
+          py="4"
+          alignItems="flex-start"
+          justifyContent="stretch"
+          backgroundColor="white">
+          <Text
+            fontSize="xl"
+            fontWeight="bold"
+            _light={{ color: 'coolGray.700' }}
+            _dark={{ color: 'coolGray.50' }}
+            mb={0}>
+            位置
+          </Text>
+          <Input
+            isRequired
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
+            py="4"
+            my="2"
+            placeholder="經緯度 (現階段僅供貼上經緯度的方式)"
+            size="lg"
+            defaultValue={formData.email}
+            onChangeText={(email: string) => setFormData((prev) => ({ ...prev, email }))}
+          />
+          <Text
+            w="100%"
+            fontSize="xl"
+            fontWeight="bold"
+            _light={{ color: 'coolGray.700' }}
+            _dark={{ color: 'coolGray.50' }}
+            mt={2}
+            mb={1}
+            onPress={() => setShowCountry(!showCountry)}>
+            縣市
+          </Text>
+          <VStack
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
+            py="4"
+            px="2"
+            my="2"
+            w="100%">
+            <Text w="100%" onPress={() => setShowCountry(!showCountry)}>
+              {country}
+            </Text>
+          </VStack>
           <Text
             fontSize="xl"
             fontWeight="bold"
@@ -334,24 +647,16 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
           </Text>
           <Input
             isRequired
-            backgroundColor="white"
-            borderColor="white"
-            borderTopWidth="0"
-            borderRightWidth="0"
-            borderLeftWidth="0"
-            borderBottomColor="coolGray.100"
-            borderBottomWidth="2"
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
             py="4"
-            mb="2"
+            my="2"
             placeholder="怎麼稱呼這個地方 (必填)"
             size="lg"
             defaultValue={formData.email}
             onChangeText={(email: string) => setFormData((prev) => ({ ...prev, email }))}
-            style={{
-              backgroundColor: 'white',
-              borderColor: 'white',
-              borderBottomColor: 'coolGray.900',
-            }}
           />
           <Text
             fontSize="xl"
@@ -361,21 +666,25 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             mb={2}>
             地點介紹
           </Text>
-          <TextArea
-            mb="4"
-            bg="white"
-            fontSize="14"
-            lineHeight="21"
-            textAlignVertical="top"
-            placeholderTextColor="coolGray.500"
-            _light={{ color: 'coolGray.800' }}
-            _dark={{ color: 'coolGray.50' }}
-            value={reason}
-            onChangeText={(txt) => handleFormUpdate('reason', txt)}
-            placeholder="如何描述這個地方 (必填)"
-            h="84"
-            autoCompleteType={undefined}
-          />
+          <VStack w="100%" px="3" py="2" mb="2" borderRadius="10" bg="coolGray.50">
+            <TextInput
+              textAlignVertical="top"
+              value={reason}
+              onChangeText={(txt) => handleFormUpdate('reason', txt)}
+              placeholderTextColor="coolGray.700"
+              placeholder="如何描述這個地方 (必填)"
+              multiline
+              numberOfLines={4}
+              style={{
+                width: '100%',
+                height: 84,
+                textAlignVertical: 'top',
+                backgroundColor: 'coolGray.300',
+                color: 'coolGray.800',
+              }}
+              autoCompleteType={undefined}
+            />
+          </VStack>
           <Text
             fontSize="xl"
             fontWeight="bold"
@@ -423,91 +732,71 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             <VStack w="100%">
               <HStack space={6} justifyContent="space-between" alignItems="center" pt={4}>
                 <Stack flexWrap="wrap" direction="row" space="2">
-                  {icons.map((item, index) => (
-                    <HStack
-                      key={'icon_' + index}
-                      overflow="visible"
-                      mx={0}
-                      mb={2}
-                      alignItems="center"
-                      justifyContent="center">
-                      <IconButton
-                        variant="unstyled"
-                        icon={
-                          <Icon
-                            as={FontAwesome5}
-                            name={item.name}
-                            _light={{
-                              color: item.active ? 'coolGray.500' : 'coolGray.200',
-                            }}
-                            _dark={{ color: 'coolGray.50' }}
-                            size={4}
-                            textAlign="center"
-                            alignSelf="center"
-                          />
-                        }
-                      />
-                      <Text
-                        fontSize={{ base: 'sm', md: 'sm' }}
-                        _light={{ color: item.active ? 'coolGray.800' : 'coolGray.300' }}
-                        _dark={{ color: { base: 'coolGray.50', md: 'coolGray.400' } }}
-                        textAlign="center">
-                        {item.text}
-                      </Text>
-                    </HStack>
-                  ))}
+                  <MasonryList
+                    showsVerticalScrollIndicator={false}
+                    numColumns={4}
+                    data={icons}
+                    renderItem={({ item }) => <FacilityCard item={item} />}
+                    keyExtractor={(item: Offer, index: number) => 'key' + index}
+                  />
                 </Stack>
-                {/*
-                <FlatList
-                  nestedScrollEnabled
-                  numColumns={4}
-                  data={icons}
-                  showsVerticalScrollIndicator={false}
-                  renderItem={({ item, index }) => (
-                    <HStack
-                      key={'icon_' + index}
-                      overflow="visible"
-                      mx={2}
-                      mb={4}
-                      alignItems="center"
-                      justifyContent="center">
-                      <IconButton
-                        variant="unstyled"
-                        icon={
-                          <Icon
-                            as={FontAwesome5}
-                            name={item.name}
-                            _light={{
-                              color: item.active ? 'coolGray.500' : 'coolGray.200',
-                            }}
-                            _dark={{ color: 'coolGray.50' }}
-                            size={4}
-                            textAlign="center"
-                            alignSelf="center"
-                          />
-                        }
-                      />
-                      <Text
-                        fontSize={{ base: 'sm', md: 'sm' }}
-                        _light={{ color: item.active ? 'coolGray.800' : 'coolGray.300' }}
-                        _dark={{ color: { base: 'coolGray.50', md: 'coolGray.400' } }}
-                        textAlign="center">
-                        {item.text}
-                      </Text>
-                    </HStack>
-                  )}
-                  keyExtractor={(item, index) => 'home-post-key-' + index}
-                />
-                      */}
               </HStack>
             </VStack>
           </HStack>
+          <Text
+            w="100%"
+            fontSize="xl"
+            fontWeight="bold"
+            _light={{ color: 'coolGray.700' }}
+            _dark={{ color: 'coolGray.50' }}
+            mt={2}
+            mb={1}
+            onPress={() => setShowAltitude(!showAltitude)}>
+            可選擇海拔 (不知道的話也沒關係)
+          </Text>
+          <VStack
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
+            py="4"
+            px="2"
+            my="2"
+            w="100%">
+            <Text w="100%" onPress={() => setShowAltitude(!showAltitude)}>
+              {altitude}
+            </Text>
+          </VStack>
+          <Text
+            w="100%"
+            fontSize="xl"
+            fontWeight="bold"
+            _light={{ color: 'coolGray.700' }}
+            _dark={{ color: 'coolGray.50' }}
+            mt={2}
+            mb={1}
+            onPress={() => setShowCrowded(!showCrowded)}>
+            可選擇擁擠程度 (不知道的話也沒關係)
+          </Text>
+          <VStack
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
+            py="4"
+            px="2"
+            my="2"
+            w="100%">
+            <Text w="100%" onPress={() => setShowCrowded(!showCrowded)}>
+              {crowded}
+            </Text>
+          </VStack>
           <Button
             variant="solid"
             size="lg"
             mt="5"
             style={{ backgroundColor: Colors.LOGO_COLOR_BROWN }}
-            onPress={() => navigation.navigate(MAIN_STACK_POST)}>
+            onPress={() => navigation.navigate(MAIN_STACK_LOCATION_DRAWER)}>
             送出
           </Button>
           <Text
@@ -520,7 +809,7 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             若審核成功將會通知您，並標記為貢獻者
           </Text>
         </VStack>
-      </Box>
+      </ScrollView>
     </DashboardLayout>
   );
 }

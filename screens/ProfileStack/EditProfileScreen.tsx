90746113802ad9ee1865d303b96bc7da20104f27
Lanar <arhspt8011@gmail.com>
2022-12-15 21:55:51 +0800

    edit profile

--- screens/ProfileStack/EditProfileScreen.tsx
+++ screens/ProfileStack/EditProfileScreen.tsx
@@ -30,35 +30,6 @@ type OptionListType = {
   planTitle: string;
 };
 
-const planOptionsList1: OptionListType[] = [
-  {
-    planTitle: '優先曝光給社團內成員',
-  },
-  {
-    planTitle: '其他說明',
-  },
-  {
-    planTitle: '其他說明',
-  },
-  {
-    planTitle: '其他說明',
-  },
-];
-const planOptionsList2: OptionListType[] = [
-  {
-    planTitle: '優先曝光給所有好友',
-  },
-  {
-    planTitle: '其他說明',
-  },
-  {
-    planTitle: '其他說明',
-  },
-  {
-    planTitle: '其他說明',
-  },
-];
-
 export default function EditProfileScreen({ navigation }: NavigationProps): JSX.Element {
   const FormComponent = () => {
     const toast = useToast();
@@ -67,14 +38,14 @@ export default function EditProfileScreen({ navigation }: NavigationProps): JSX.
     const [isTimePickerVisible, setTimePickerVisibility] = useState(false); //TIME PICKER
 
     type FormData = {
-      title: string;
       email: string;
-      password: string;
+      nick: string;
+      description: string;
     };
     const [formData, setFormData] = useState<FormData>({
-      title: '',
       email: '',
-      password: '',
+      nick: '',
+      description: '',
     });
 
     return (
@@ -100,8 +71,8 @@ export default function EditProfileScreen({ navigation }: NavigationProps): JSX.
             mb="2"
             placeholder="請輸入電子郵件"
             size="lg"
-            defaultValue={formData.title}
-            onChangeText={(title: string) => setFormData((prev) => ({ ...prev, title }))}
+            defaultValue={formData.email}
+            onChangeText={(email: string) => setFormData((prev) => ({ ...prev, email }))}
           />
           <HStack alignItems="flex-end">
             <Text fontSize="lg" fontWeight="bold" color="coolGray.700" mb={0} mr={2}>
@@ -122,10 +93,37 @@ export default function EditProfileScreen({ navigation }: NavigationProps): JSX.
             mb="2"
             placeholder="請輸入暱稱"
             size="lg"
-            defaultValue={formData.title}
-            onChangeText={(title: string) => setFormData((prev) => ({ ...prev, title }))}
+            defaultValue={formData.nick}
+            onChangeText={(nick: string) => setFormData((prev) => ({ ...prev, nick }))}
           />
-
+          <HStack alignItems="flex-end">
+            <Text fontSize="lg" fontWeight="bold" color="coolGray.700" mb={0} mr={2}>
+              個人簡介
+            </Text>
+            <Text fontSize="sm" fontWeight="normal" color="coolGray.500">
+              選填
+            </Text>
+          </HStack>
+          <VStack w="100%" px="3" py="2" mb="2" borderRadius="10" bg="coolGray.50">
+            <TextInput
+              textAlignVertical="top"
+              value={formData.description}
+              onChangeText={(description: string) =>
+                setFormData((prev) => ({ ...prev, description }))
+              }
+              placeholderTextColor="coolGray.700"
+              placeholder="如何描述這個地方 (必填)"
+              multiline
+              numberOfLines={4}
+              style={{
+                width: '100%',
+                height: 84,
+                textAlignVertical: 'top',
+                backgroundColor: 'coolGray.300',
+                color: 'coolGray.800',
+              }}
+            />
+          </VStack>
           <Center width="100%">
             <Button
               width="100%"

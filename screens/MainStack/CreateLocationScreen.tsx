8f987146cb0520179c8e7d1d27a2bcad285f66c1
Lanar <arhspt8011@gmail.com>
2022-12-15 21:14:50 +0800

    create location picker

--- screens/MainStack/CreateLocationScreen.tsx
+++ screens/MainStack/CreateLocationScreen.tsx
@@ -1,5 +1,4 @@
 import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
-import { Picker } from '@react-native-picker/picker';
 import MasonryList from '@react-native-seoul/masonry-list';
 import * as ImagePicker from 'expo-image-picker';
 import {
@@ -16,10 +15,11 @@ import {
   Image,
   Stack,
   IconButton,
-  CheckIcon,
+  View,
 } from 'native-base';
 import React, { useState } from 'react';
 import { Keyboard, Dimensions, ScrollView, TouchableOpacity, TextInput } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
 import { MAIN_STACK_LOCATION_DRAWER } from '../../NavigationNames';
 import { NavigationProps } from '../../Props';
@@ -70,197 +70,12 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
   });
 
   const [showCountry, setShowCountry] = useState(false);
-  const [showAltitude, setShowAltitude] = useState(false);
-  const [showRecommend, setShowRecommend] = useState(false);
-  const [showCrowded, setShowCrowded] = useState(false);
   const [country, setCountry] = React.useState('');
   const [altitude, setAltitude] = useState('');
   const [recommend, setRecommend] = useState('');
   const [crowded, setCrowded] = useState('');
+  const [bugs, setBugs] = useState('');
 
-  function PlanDescriptionItem({ planTitle }: { planTitle: string }) {
-    return (
-      <HStack space="3" w={{ base: '100%', md: '50%' }} mb="5">
-        <Icon
-          size={6}
-          as={MaterialIcons}
-          name="check-circle"
-          _light={{
-            color: 'amber.900',
-          }}
-          _dark={{
-            color: 'primary.500',
-          }}
-        />
-        <Text
-          flex={1}
-          fontSize="sm"
-          _light={{
-            color: 'coolGray.800',
-          }}
-          _dark={{ color: 'coolGray.50' }}
-          fontWeight="normal"
-          lineHeight="21">
-          {planTitle}
-        </Text>
-      </HStack>
-    );
-  }
-
-  function PlanOption() {
-    return (
-      <Box>
-        <HStack alignItems="center" mt={{ base: 7, md: 8 }} mb={5} space={1.5}>
-          <Text
-            _light={{ color: 'primary.900' }}
-            _dark={{ color: 'primary.500' }}
-            fontSize="xl"
-            fontWeight="bold">
-            50% off
-          </Text>
-          <Text
-            _light={{ color: 'coolGray.500' }}
-            _dark={{ color: 'coolGray.400' }}
-            fontSize="sm"
-            fontWeight="medium">
-            Until Sunday, June 25th
-          </Text>
-        </HStack>
-        <HStack flexWrap="wrap">
-          {planOptionsList1.map((plan, index) => (
-            <PlanDescriptionItem key={index} {...plan} />
-          ))}
-        </HStack>
-      </Box>
-    );
-  }
-
-  function TabItem({
-    tabName,
-    currentTab,
-    handleTabChange,
-  }: {
-    tabName: string;
-    currentTab: string;
-    handleTabChange: (tabTitle: string) => void;
-  }) {
-    const getCurrentTabStyle = () => {
-      return tabName === currentTab
-        ? {
-            borderRadius: 'lg',
-            _light: {
-              bg: Colors.LOGO_COLOR_BROWN,
-              color: 'coolGray.50',
-            },
-            _dark: {
-              bg: 'primary.500',
-              color: 'coolGray.50',
-            },
-          }
-        : {
-            _light: {
-              bg: 'warning.50',
-              color: 'coolGray.500',
-            },
-            _dark: {
-              bg: 'coolGray.700',
-              color: 'coolGray.400',
-            },
-          };
-    };
-    return (
-      <Pressable onPress={() => handleTabChange(tabName)} flex={1}>
-        <Text
-          textAlign="center"
-          fontSize="lg"
-          fontWeight="medium"
-          letterSpacing="0.4"
-          {...getCurrentTabStyle()}
-          py={4}>
-          {tabName}
-        </Text>
-      </Pressable>
-    );
-  }
-
-  function OptionSection() {
-    const [tabName, setTabName] = React.useState('社團活動');
-
-    return (
-      <>
-        <HStack mt={5} alignItems="center" width="100%" borderRadius="sm" overflow="hidden">
-          <TabItem
-            tabName="社團活動"
-            currentTab={tabName}
-            handleTabChange={(tab) => setTabName(tab)}
-          />
-          <TabItem
-            tabName="個人活動"
-            currentTab={tabName}
-            handleTabChange={(tab) => setTabName(tab)}
-          />
-        </HStack>
-        {tabName === '社團活動' ? (
-          <Box>
-            <HStack alignItems="center" mt={{ base: 7, md: 8 }} mb={5} space={1.5}>
-              <Text
-                _light={{ color: 'amber.900' }}
-                _dark={{ color: 'primary.500' }}
-                fontSize="xl"
-                fontWeight="bold">
-                在社團內舉辦活動
-              </Text>
-              <Text
-                _light={{ color: 'coolGray.500' }}
-                _dark={{ color: 'coolGray.400' }}
-                fontSize="sm"
-                fontWeight="medium">
-                號召團員們參加
-              </Text>
-            </HStack>
-            <HStack flexWrap="wrap">
-              {planOptionsList1.map((plan, index) => (
-                <PlanDescriptionItem key={index} {...plan} />
-              ))}
-            </HStack>
-            <Text
-              _light={{ color: 'coolGray.500' }}
-              _dark={{ color: 'coolGray.400' }}
-              fontSize="sm"
-              fontWeight="medium">
-              審核是否通過依據該社團管理員決定
-            </Text>
-          </Box>
-        ) : tabName === '個人活動' ? (
-          <Box>
-            <HStack alignItems="center" mt={{ base: 7, md: 8 }} mb={5} space={1.5}>
-              <Text
-                _light={{ color: 'amber.900' }}
-                _dark={{ color: 'primary.500' }}
-                fontSize="xl"
-                fontWeight="bold">
-                舉辦個人活動
-              </Text>
-              <Text
-                _light={{ color: 'coolGray.500' }}
-                _dark={{ color: 'coolGray.400' }}
-                fontSize="sm"
-                fontWeight="medium">
-                邀請你的朋友們參加
-              </Text>
-            </HStack>
-            <HStack flexWrap="wrap">
-              {planOptionsList2.map((plan, index) => (
-                <PlanDescriptionItem key={index} {...plan} />
-              ))}
-            </HStack>
-          </Box>
-        ) : (
-          <></>
-        )}
-      </>
-    );
-  }
   type DeactivateForm = {
     reason: string;
     password: string;
@@ -457,131 +272,9 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
 
   return (
     <DashboardLayout title="新增地點" showBackButton>
-      {showCountry && (
-        <VStack
-          position="absolute"
-          bg="white"
-          bottom={0}
-          left={0}
-          w="100%"
-          h="240"
-          pb="30"
-          shadow={2}
-          zIndex={9}
-          flex={1}
-          justifyContent="center">
-          <Picker
-            mode="dialog"
-            style={{ height: 100, flex: 1 }}
-            accessibilityLabel="選擇縣市"
-            placeholder="選擇縣市"
-            selectedValue={country}
-            onValueChange={(itemValue, itemIndex) => {
-              setCountry(itemValue);
-            }}>
-            <Picker.Item label="台北市" value="台北" />
-            <Picker.Item label="新北市" value="新北" />
-            <Picker.Item label="桃園市" value="桃園" />
-            <Picker.Item label="台中市" value="台中" />
-            <Picker.Item label="待串接整合縣市" value="" />
-          </Picker>
-          <Button
-            mx={4}
-            variant="solid"
-            size="lg"
-            style={{ backgroundColor: 'black' }}
-            onPress={() => {
-              setShowCountry(false);
-            }}>
-            選擇
-          </Button>
-        </VStack>
-      )}
-      {showAltitude && (
-        <VStack
-          position="absolute"
-          bg="white"
-          bottom={0}
-          left={0}
-          w="100%"
-          h="240"
-          pb="30"
-          shadow={2}
-          zIndex={9}
-          flex={1}
-          justifyContent="center">
-          <Picker
-            mode="dialog"
-            style={{ height: 100, flex: 1 }}
-            accessibilityLabel="選擇海拔"
-            placeholder="選擇海拔"
-            selectedValue={altitude}
-            onValueChange={(itemValue, itemIndex) => {
-              setAltitude(itemValue);
-            }}>
-            <Picker.Item label="不確定" value="" />
-            <Picker.Item label="海邊" value="海邊" />
-            <Picker.Item label="平地" value="平地" />
-            <Picker.Item label="300公尺以下" value="300" />
-            <Picker.Item label="300公尺~500公尺" value="500" />
-            <Picker.Item label="500公尺~800公尺" value="800" />
-            <Picker.Item label="800公尺~1000公尺" value="1000" />
-            <Picker.Item label="1000公尺以上" value="1100" />
-          </Picker>
-          <Button
-            mx={4}
-            variant="solid"
-            size="lg"
-            style={{ backgroundColor: 'black' }}
-            onPress={() => {
-              setShowAltitude(false);
-            }}>
-            選擇
-          </Button>
-        </VStack>
-      )}
-      {showCrowded && (
-        <VStack
-          position="absolute"
-          bg="white"
-          bottom={0}
-          left={0}
-          w="100%"
-          h="240"
-          pb="30"
-          shadow={2}
-          zIndex={9}
-          flex={1}
-          justifyContent="center">
-          <Picker
-            mode="dialog"
-            style={{ height: 100, flex: 1 }}
-            accessibilityLabel="選擇擁擠程度"
-            placeholder="選擇擁擠程度"
-            selectedValue={crowded}
-            onValueChange={(itemValue, itemIndex) => {
-              setCrowded(itemValue);
-            }}>
-            <Picker.Item label="空曠" value="空曠" />
-            <Picker.Item label="偶爾" value="偶爾" />
-            <Picker.Item label="總是" value="總是" />
-          </Picker>
-          <Button
-            mx={4}
-            variant="solid"
-            size="lg"
-            style={{ backgroundColor: 'black' }}
-            onPress={() => {
-              setShowCrowded(false);
-            }}>
-            選擇
-          </Button>
-        </VStack>
-      )}
       <ScrollView
         onPress={() => {
           Keyboard.dismiss();
-          setShowCountry(false);
         }}>
         <VStack
           space="0"
@@ -618,8 +311,7 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             _light={{ color: 'coolGray.700' }}
             _dark={{ color: 'coolGray.50' }}
             mt={2}
-            mb={1}
-            onPress={() => setShowCountry(!showCountry)}>
+            mb={1}>
             縣市
           </Text>
           <VStack
@@ -631,9 +323,22 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             px="2"
             my="2"
             w="100%">
-            <Text w="100%" onPress={() => setShowCountry(!showCountry)}>
-              {country}
-            </Text>
+            <View ml="1">
+              {/*Better phone input component https://github.com/rili-live/react-native-phone-input*/}
+              <RNPickerSelect
+                placeholder={{}}
+                textInputProps={{ fontSize: 14, color: 'coolGray.400' }}
+                value={country}
+                onValueChange={(itemValue) => setCountry(itemValue)}
+                items={[
+                  { label: '請選擇', value: '' },
+                  { label: '台北市', value: '台北市' },
+                  { label: '新北市', value: '新北市' },
+                  { label: '桃園市', value: '桃園市' },
+                  { label: '台中市', value: '台中市' },
+                ]}
+              />
+            </View>
           </VStack>
           <Text
             fontSize="xl"
@@ -723,7 +428,7 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             _light={{ color: 'coolGray.700' }}
             _dark={{ color: 'coolGray.50' }}
             mb={0}>
-            選擇此地點提供與未提供的設施
+            可選擇提供與未提供的設施
           </Text>
           <HStack justifyContent="space-between">
             <VStack w="100%">
@@ -747,9 +452,8 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             _light={{ color: 'coolGray.700' }}
             _dark={{ color: 'coolGray.50' }}
             mt={2}
-            mb={1}
-            onPress={() => setShowAltitude(!showAltitude)}>
-            可選擇海拔 (不知道的話也沒關係)
+            mb={1}>
+            可選擇海拔 (選填)
           </Text>
           <VStack
             backgroundColor="coolGray.50"
@@ -760,9 +464,25 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             px="2"
             my="2"
             w="100%">
-            <Text w="100%" onPress={() => setShowAltitude(!showAltitude)}>
-              {altitude}
-            </Text>
+            <View ml="1">
+              {/*Better phone input component https://github.com/rili-live/react-native-phone-input*/}
+              <RNPickerSelect
+                placeholder={{}}
+                textInputProps={{ fontSize: 14, color: 'coolGray.400' }}
+                value={altitude}
+                onValueChange={(itemValue) => setAltitude(itemValue)}
+                items={[
+                  { label: '不確定', value: '' },
+                  { label: '海邊', value: '海邊' },
+                  { label: '平地', value: '平地' },
+                  { label: '300公尺以下', value: '300' },
+                  { label: '300公尺~500公尺', value: '500' },
+                  { label: '500公尺~800公尺', value: '800' },
+                  { label: '800公尺~1000公尺', value: '1000' },
+                  { label: '1000公尺以上', value: '1100' },
+                ]}
+              />
+            </View>
           </VStack>
           <Text
             w="100%"
@@ -771,9 +491,8 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             _light={{ color: 'coolGray.700' }}
             _dark={{ color: 'coolGray.50' }}
             mt={2}
-            mb={1}
-            onPress={() => setShowCrowded(!showCrowded)}>
-            可選擇擁擠程度 (不知道的話也沒關係)
+            mb={1}>
+            可選擇擁擠程度 (選填)
           </Text>
           <VStack
             backgroundColor="coolGray.50"
@@ -784,9 +503,59 @@ export default function CreateLocationScreen({ navigation }: NavigationProps): J
             px="2"
             my="2"
             w="100%">
-            <Text w="100%" onPress={() => setShowCrowded(!showCrowded)}>
-              {crowded}
-            </Text>
+            <View ml="1">
+              {/*Better phone input component https://github.com/rili-live/react-native-phone-input*/}
+              <RNPickerSelect
+                placeholder={{}}
+                textInputProps={{ fontSize: 14, color: 'coolGray.400' }}
+                value={crowded}
+                onValueChange={(itemValue) => setCrowded(itemValue)}
+                items={[
+                  { label: '不確定', value: '' },
+                  { label: '空曠', value: '空曠' },
+                  { label: '偶爾', value: '偶爾' },
+                  { label: '總是', value: '總是' },
+                ]}
+              />
+            </View>
+          </VStack>
+          <Text
+            w="100%"
+            fontSize="xl"
+            fontWeight="bold"
+            _light={{ color: 'coolGray.700' }}
+            _dark={{ color: 'coolGray.50' }}
+            mt={2}
+            mb={1}>
+            可選擇蚊蟲程度 (選填)
+          </Text>
+          <VStack
+            backgroundColor="coolGray.50"
+            borderColor="coolGray.50"
+            borderRadius="10"
+            borderWidth="0"
+            py="4"
+            px="2"
+            my="2"
+            w="100%">
+            <View ml="1">
+              {/*Better phone input component https://github.com/rili-live/react-native-phone-input*/}
+              <RNPickerSelect
+                placeholder={{}}
+                textInputProps={{ fontSize: 14, color: 'coolGray.400' }}
+                value={bugs}
+                onValueChange={(itemValue) => setBugs(itemValue)}
+                items={[
+                  { label: '不確定', value: '' },
+                  { label: '少蚊蟲', value: '少蚊蟲' },
+                  { label: '小黑蚊多', value: '小黑蚊多' },
+                  { label: '大蚊子多', value: '大蚊子多' },
+                  { label: '小黑蚊多、大蚊子少', value: '小黑蚊多、大蚊子少' },
+                  { label: '小黑蚊少、大蚊子多', value: '小黑蚊少、大蚊子多' },
+                  { label: '都很多', value: '都很多' },
+                ]}
+              />
+            </View>
           </VStack>
           <Center width="100%">
             <Button
